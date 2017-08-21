812cde6eac8148da356bffe6f0561128463e9010
Zachary Golba <zachary.golba@postlight.com>
2017-08-21 17:56:02 -0400

    feat: emit both commonjs and es modules from build (#5)

--- rollup.config.js
+++ rollup.config.js
@@ -6,10 +6,18 @@ import resolve from 'rollup-plugin-node-resolve'
 
 export default {
   input: path.join(__dirname, 'src', 'index.js'),
-  output: {
-    file: path.join(__dirname, 'dist', 'index.js'),
-    format: 'cjs',
-  },
+  output: [
+    {
+      file: path.join(__dirname, 'dist', 'index.js'),
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: path.join(__dirname, 'dist', 'index.es.js'),
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
   plugins: [
     resolve(),
     babel(),
