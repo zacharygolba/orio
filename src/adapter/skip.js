79e9e122c907e0356535e94f4da6889645f78d40
Zachary Golba <zachary.golba@postlight.com>
2017-08-27 16:53:34 -0400

    refactor: remove abnormal logic in skip while adapter

--- src/adapter/skip.js
+++ src/adapter/skip.js
@@ -5,20 +5,20 @@ import { ProducerBase } from '../producer'
 export default class SkipAdapter<T> extends ProducerBase<T> {
   amount: number
   producer: Iterator<T>
-  state: number
+  calls: number
 
   constructor(producer: Iterator<T>, amount: number) {
     super()
     this.amount = amount
+    this.calls = 0
     this.producer = producer
-    this.state = 0
   }
 
   next(): IteratorResult<T, void> {
     const next = this.producer.next()
 
-    if (this.state < this.amount) {
-      this.state += 1
+    if (this.calls < this.amount) {
+      this.calls += 1
       return this.next()
     }
 
