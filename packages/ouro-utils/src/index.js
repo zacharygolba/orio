5a39d2b9af23e4868bb5beefd77a727b520f346a
Zachary Golba <zachary.golba@postlight.com>
2017-09-09 13:21:48 -0400

    refactor: remove unicode module from ouro-utils

--- packages/ouro-utils/src/index.js
+++ packages/ouro-utils/src/index.js
@@ -1,10 +1,7 @@
 // @flow
 
-import * as unicode from './unicode'
-import identity from './identity'
-
+export { default as identity } from './identity'
 export * from './consts'
 export * from './iterable'
 export * from './reduce'
 export * from './timers'
-export { identity, unicode }
